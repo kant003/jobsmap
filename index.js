c40ea55b37f93d1819a6ca6ce3c7f4adfca0ea57
kant003 <gonzalezm.angel@gmail.com>
2023-05-30 15:00:57 +0200

    finish

--- index.js
+++ index.js
@@ -1,58 +1,59 @@
-import express from "express";
-import * as dotenv from "dotenv";
-import morgan from "morgan";
-import cors from "cors";
-import { getOffers, getLatLon, filterString } from "./services.js";
+import express from 'express'
+import * as dotenv from 'dotenv'
+import morgan from 'morgan'
+import cors from 'cors'
+import { getOffers, getLatLon, filterString } from './services.js'
 
-dotenv.config();
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+dotenv.config()
+const app = express()
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+app.use(cors())
+app.use(express.static('public'))
+app.use(morgan('tiny'))
 
-app.use(cors());
+app.get('/hello', (req, res) => {
+  res.status(200).send('Bienvenido al API de infobos JobsMap')
+})
 
-app.use(express.static("public"));
+app.get('/', (req, res) => {
+  res.redirect('/map.html')
+})
 
-app.use(morgan("tiny"));
+app.get('/jobs', async (req, res) => {
+  const category = req.query.category || 'informatica-telecomunicaciones'
+  console.log('buscando:', category)
 
-app.get("/hello", (req, res) => {
-  res.status(200).send("Bienvenido al API de infobos JobsMap");
-});
-
-app.get("/", (req, res) => {
-  res.redirect("/map.html");
-});
-
-app.get("/jobs", async (req, res) => {
-  const category = req.query.category || "informatica-telecomunicaciones";
-  console.log("buscando:", category);
-
-  const offer = await getOffers(category);
-  let data = offer.items.map((item) => {
-    item.city = filterString(item.city);
-    item.province.value = filterString(item.province.value);
+  const offer = await getOffers(category)
+  const data = offer.items.map((item) => {
+    item.city = filterString(item.city)
+    item.province.value = filterString(item.province.value)
 
     return {
       title: item.title,
       city: item.city,
       province: item.province.value,
       link: item.link,
-      contract: item.contractType.value,
-    };
-  });
+      contract: item.contractType?.value,
+      experienceMin: item.experienceMin?.value,
+      study: item.study?.value,
+      workDay: item.workDay?.value,
+      teleworking: item.teleworking?.value
+    }
+  })
 
-  for (let item of data) {
-    const pos = await getLatLon(`${item.city} ${item.province}`);
-    //onst pos={lat: 10, lon: 11}
-    item.lat = pos.lat;
-    item.lng = pos.lon;
+  for (const item of data) {
+    const pos = await getLatLon(`${item.city}`)
+    // const pos={lat: 10, lon: 11}
+    item.lat = pos.lat
+    item.lng = pos.lon
   }
 
-  res.status(200).send(data);
-});
+  res.status(200).send(data)
+})
 
 async function main() {
-  await app.listen(process.env.PORT);
-  console.log("Servidor corriendo en el puerto " + process.env.PORT);
+  await app.listen(process.env.PORT)
+  console.log('Servidor corriendo en el puerto ' + process.env.PORT)
 }
-main().catch((error) => console.error("Fallo al arrancar el servidor" + error));
+main().catch((error) => console.error('Fallo al arrancar el servidor' + error))
