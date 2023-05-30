f867a2079ef04cf106e1ec3d6074079c4bcb4260
kant003 <gonzalezm.angel@gmail.com>
2023-05-30 03:51:25 +0200

    default category

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ app.use(express.static('public'))
 
 app.use(morgan("tiny"));
 
-
+app.get("/hello", (req, res) => {
+    res.status(200).send("Bienvenido al API de infobos JobsMap");
+    
+  
+  });
 
 app.get("/", (req, res) => {
   //res.status(200).send("Bienvenido al API de infobos JobsMap");
